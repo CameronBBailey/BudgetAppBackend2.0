fbf3be76aa2efe3250549dbab5a4ee7025efc955
CameronBBailey <CameronBradBailey@gmail.com>
2024-05-14 19:41:31 -0400

    first commit

---
+++ src/main/java/com/example/budgetappbackend/BudgetAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budgetappbackend/BudgetAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
